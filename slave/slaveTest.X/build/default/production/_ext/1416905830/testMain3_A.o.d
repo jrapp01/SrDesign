dcd9e16c62e3c78eb3230e1da84d32014d455d56
jrapp01 <joshua.rapp@tufts.edu>
2014-04-30 21:34:57 -0400

    Final Changes

--- slave/slaveTest.X/build/default/production/_ext/1416905830/testMain3_A.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1416905830/testMain3_A.o:  \
- C:/Users/Administrator/Documents/GitHub/SrDesign/slave/src/testMain3_A.c  \
- ../include/math.h  \
-../include/delay.h 
---
+++ slave/slaveTest.X/build/default/production/_ext/1416905830/testMain3_B.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1416905830/testMain3_B.o:  \
+ C:/Users/Administrator/Documents/GitHub/SrDesign/slave/src/testMain3_B.c  \
+ ../include/math.h  \
+../include/delay.h 
