c7bfa522a6e86d96752a1db5304e2c038b2c5f41
jrapp01 <joshua.rapp@tuft.edu>
2014-05-05 18:18:43 -0400

    Unimportant (ignore)

--- slave/src/testMain1_F.c
+++ slave/src/testMain1_F.c
@@ -1,19 +1,5 @@
 /*-----------------------------------------------------------------------------*
  *
- *   Copyright 2014 Tom Cahill
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
  *   File: main.c
  *
  *   Description: Includes hardware configuration, functions to send note data
@@ -62,19 +48,13 @@
 #define INITCH  (ADC_CH0_POS_SAMPLEA_AN0 | ADC_CH0_NEG_SAMPLEA_NVREF)
 
 //ADC Data
-
-int max[16]={0};            //Value of each analog input at maximum displacement
-int min[16]={0};            //Value of each analog input at rest
-//int samp1[16];              //20% from rest
-//int samp2[16];              //80% from rest
 int count = 0;
 
 //MIDI PMP data
-short txFlag; //causes interrupt in master when data ready
-short txBuff[16]; //contains MIDI data for master to send over USB
+short txFlag;       //causes interrupt in master when data ready
+short txBuff[16];   //contains MIDI data for master to send over USB
 
 long long timer;            //For velocity timing; updated by core timer
-//#define VEL_SCALE 4         //TODO: figure out optimal value for this
 
 // Parallel Master Port Configuration
 #define PMP_CONTROL        PMP_READ_POL_LO | PMP_WRITE_POL_LO | PMP_CS2_CS1_EN
@@ -85,20 +65,17 @@ long long timer;            //For velocity timing; updated by core timer
 // Change Notice Config
 // IOPORT bit masks can be found in ports.h
 #define CONFIG          (CN_ON)
-//#define PINS            (CN16_ENABLE)
 #define PINS (CN0_ENABLE | CN1_ENABLE | CN8_ENABLE | CN9_ENABLE | \
 		CN10_ENABLE | CN11_ENABLE | CN15_ENABLE | CN16_ENABLE | \
 		CN17_ENABLE | CN18_ENABLE | CN19_ENABLE | CN20_ENABLE | \
 		CN21_ENABLE)
-//#define PULLUPS         (CN15_PULLUP_ENABLE | CN16_PULLUP_ENABLE)
 #define PULLUPS (CN_PULLUP_DISABLE_ALL)
-//#define INTERRUPT       (CHANGE_INT_ON | CHANGE_INT_PRI_2)
 #define INTERRUPT (CHANGE_INT_ON | CHANGE_INT_PRI_1)
 
 //Key states
 typedef enum{
-    GET_ON, //State for collecting note-on information
-    GET_NEXT
+    GET_ON,     //State for collecting note-on information
+    GET_NEXT    //State to denoise, wait until signal below threshold
 }state;
 state keyState[16]={GET_ON};
 
@@ -111,16 +88,21 @@ unsigned int initialF = 0;
 unsigned int initialG = 0;
 
 const int threshold = 275;       //TODO: Optimize arbitrary threshold setting
-int velocity[16][2]={0};        //Record timestamps to extract note-on velocity
+int velocity[16][2]={0};        //Extract note-on velocity
 int forceData[16][2]={0};       //Collect FSR hammer force data
-int slopeData[16][2]={0};       //Contrain calculated slope data
+int slopeData[16][2]={0};       //Contain calculated slope data
 
 void SendNotes(int note){
     mPORTDSetBits(BIT_8);
+    
     if((txFlag & (1<<note))>>note){
         while(!mIsPMPSlaveBufferEmpty());
+        
+        //write note number to master
         mPMPSlaveWrite(note);
         while(!mIsPMPSlaveBufferEmpty());
+        
+        //write note velocity to master
         mPMPSlaveWrite(txBuff[note]);
         txFlag -= (1 << note);
     }
@@ -129,15 +111,19 @@ void SendNotes(int note){
 
 void noteOn(int note, int velocity){
     txFlag |= 1 << note;
-    //velocity /= VEL_SCALE;
+    
+    //velocity scaling based on input dynamic range
     velocity = (velocity-247)/3;
+    
+    //if value over 127, hard limit to max velocity
     velocity = (velocity > (0x80)) ? 127 : velocity;
-    //txBuff[note]=(short)(127-velocity); //not an inverse relationship in FSR system
+    
     txBuff[note]=(short)(velocity);
     SendNotes(note);
 }
 
 void noteOff(int note){
+    //send note on w/ velocity  = 0
     txFlag |= 1 << note;
     txBuff[note] = 0;
     SendNotes(note);
@@ -201,37 +187,11 @@ void __ISR(_TIMER_2_VECTOR, IPL5) T2Handle(void){
 }
 
 int main(void){
-    //CN STEP 1A. Disable all interrupts
-    //unsigned int status;
-    //status = INTDisableInterrupts();//mINTDisableSystemMultiVectorInt();
-/*
-    //CN STEP 1B. Set pins for digital inputs
-    mPORTCSetPinsDigitalIn(BIT_13 | BIT_14); //CN0,1
-    mPORTDSetPinsDigitalIn(BIT_6 | BIT_7 | BIT_13 | BIT_14 | BIT_15); //CN15,16,19,20,21
-    mPORTFSetPinsDigitalIn(BIT_4 | BIT_5); //CN17,18
-    mPORTGSetPinsDigitalIn(BIT_6 | BIT_7 | BIT_8 | BIT_9); //CN8,9,10,11
-*/
-  //  mPORTASetPinsDigitalIn(0xFFFF); //Enable all port A pins as digital inputs
+
     mPORTBSetPinsAnalogIn(0xFFFF);             //Enable all analog
     mPORTDSetPinsDigitalOut(BIT_0 | BIT_1 | BIT_8);
-  
-    //Buttons 1 and 2 enabled
-    //PORTSetPinsDigitalIn(IOPORT_D, BIT_6 | BIT_7);
     
     SYSTEMConfig(SYS_FREQ, SYS_CFG_WAIT_STATES | SYS_CFG_PCACHE);
-
-    //CN STEP 2. setup the change notice options
-	//mCNOpen(CONFIG, PINS, PULLUPS);
-/*
-    //CN STEP 3. read port(s) to clear mismatch
-    initialC = mPORTCReadBits(BIT_13 | BIT_14);
-    initialD = mPORTDReadBits(BIT_6 | BIT_7 | BIT_13 | BIT_14 | BIT_15);
-    initialF = mPORTFReadBits(BIT_4 | BIT_5);
-    initialG = mPORTGReadBits(BIT_6 | BIT_7 | BIT_8 | BIT_9);
-
-    //CN STEP 4. clear change notice interrupt flag
-    ConfigIntCN(INTERRUPT);
-*/
 
     CloseADC10();
     SetChanADC10(INITCH);
@@ -239,6 +199,7 @@ int main(void){
     ConfigIntADC10(CFGINT);
     EnableADC10();
 
+    //
     ConfigINT0(EXT_INT_PRI_6 | RISING_EDGE_INT | EXT_INT_ENABLE);
     ConfigINT1(EXT_INT_PRI_6 | RISING_EDGE_INT | EXT_INT_ENABLE);
     ConfigINT2(EXT_INT_PRI_6 | RISING_EDGE_INT | EXT_INT_ENABLE);
@@ -249,7 +210,7 @@ int main(void){
 
     mPORTDClearBits(BIT_0 | BIT_1 | BIT_8);
 
-        // Enable change notice, enable discrete pins and weak pullups
+    // Enable change notice, enable discrete pins and weak pullups
     //mCNOpen(CONFIG, PINS, PULLUPS);
 
     // Read the port to clear any mismatch on change notice pins
